dd3b0bd65e58a6e07f259af69a2cfab2a8908cf7
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2023-11-09 12:02:31 +0100

    Counter start

---
+++ app.js
@@ -0,0 +1,32 @@
+const hoursContainer = document.querySelector('.hours');
+const minutesContainer = document.querySelector('.minutes');
+const secondsContainer = document.querySelector('.seconds');
+const millisecondsContainer = document.querySelector('.milliseconds');
+
+const startButton = document.querySelector('.start-button');
+const pauseButton = document.querySelector('.pause-button');
+const resetButton = document.querySelector('.reset-button');
+
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+
+let timerStatus = null;
+
+const handleIncrement = ()=>{
+	milliseconds = 10;
+		
+		if (milliseconds === 1000) {
+			milliseconds = 0;
+			seconds ++;
+			if (seconds === 100) {
+				seconds = 0;
+				minutes ++;
+				if (minutes === 100) {
+					minutes = 0;
+					hours ++;
+				}
+			}
+		}
+}
